f0df901078f4b576968d6d1734b9b550b09cbd15
JBelmont89 <38103692+JBelmont89@users.noreply.github.com>
2025-10-05 20:41:09 +0100

    add BES workshop

--- docs/BES_workshop/practical1_compiler.R
+++ docs/BES_workshop/practical1_compiler.R
@@ -1,4 +1,4 @@
-## ----child= "C:\\Users\\jb538u\\OneDrive - University of Glasgow\\Documents\\inlabru_workshop2025\\inlabru_workshop\\docs\\practicals\\LM_ex.qmd"----
+## ----child="practicals\\LM_ex.qmd"--------------------------------------------
 
 ## -----------------------------------------------------------------------------
 #| warning: false
@@ -89,7 +89,85 @@ pred %>% ggplot() +
 
 
 
-## ----child= "C:\\Users\\jb538u\\OneDrive - University of Glasgow\\Documents\\inlabru_workshop2025\\inlabru_workshop\\docs\\practicals\\GLM_ex.qmd"----
+## ----child="practicals\\LMM_ex.qmd"-------------------------------------------
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Simulate data from a LMM"
+#| 
+set.seed(12)
+beta = c(1.5,1)
+sd_error = 1
+tau_group = 1
+
+n = 100
+n.groups = 5
+x = rnorm(n)
+v = rnorm(n.groups, sd = tau_group^{-1/2})
+y = beta[1] + beta[2] * x + rnorm(n, sd = sd_error) +
+  rep(v, each = 20)
+
+df = data.frame(y = y, x = x, j = rep(1:5, each = 20))  
+
+
+## ----plot_data_lmm------------------------------------------------------------
+#| code-fold: true
+#| fig-cap: Data for the linear mixed model example with 5 groups
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+
+ggplot(df) +
+  geom_point(aes(x = x, colour = factor(j), y = y)) +
+  theme_classic() +
+  scale_colour_discrete("Group")
+
+
+
+## ----define_components_lmm----------------------------------------------------
+# Define model components
+cmp =  ~ -1 + beta_0(1) + beta_1(x, model = "linear") +
+  u(j, model = "iid")
+
+
+## ----define_likelihood_lmm----------------------------------------------------
+# Construct likelihood
+lik =  like(formula = y ~.,
+            family = "gaussian",
+            data = df)
+
+
+## -----------------------------------------------------------------------------
+#| collapse: true
+#| code-summary: "Fit a LMM in inlabru"
+fit = bru(cmp, lik)
+summary(fit)
+
+
+## -----------------------------------------------------------------------------
+#| code-fold: true
+#| code-summary: "LMM fitted values"
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+
+# New data
+xpred = seq(range(x)[1], range(x)[2], length.out = 100)
+j = 1:n.groups
+pred_data = expand.grid(x = xpred, j = j)
+pred = predict(fit, pred_data, formula = ~ beta_0 + beta_1 + u) 
+
+
+pred %>%
+  ggplot(aes(x=x,y=mean,color=factor(j)))+
+  geom_line()+
+  geom_ribbon(aes(x,ymin = q0.025, ymax= q0.975,fill=factor(j)), alpha = 0.5) + 
+  geom_point(data=df,aes(x=x,y=y,colour=factor(j)))+
+  facet_wrap(~j)
+
+
+
+
+## ----child="practicals\\GLM_ex.qmd"-------------------------------------------
 
 ## -----------------------------------------------------------------------------
 #| code-summary: "Simulate Data from a GLM"
@@ -173,19 +251,75 @@ df_logis = data.frame(y = y, w = w)
 
 
 
-## ----child= "C:\\Users\\jb538u\\OneDrive - University of Glasgow\\Documents\\inlabru_workshop2025\\inlabru_workshop\\docs\\practicals\\spatial_data_types.qmd"----
+## ----child="practicals\\GAM_ex.qmd"-------------------------------------------
 
 ## -----------------------------------------------------------------------------
-#| warning: false
+#| echo: false
 #| message: false
-#| code-summary: "Load libraries"
-
+#| warning: false
+library(webexercises)
 library(dplyr)
 library(INLA)
 library(ggplot2)
 library(patchwork)
 library(inlabru)     
-# load some libraries to generate nice plots
+# load some libraries to generate nice map plots
 library(scico)
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Simulate GAM Data"
+n = 100
+x = rnorm(n)
+eta = (1 + cos(x))
+y = rnorm(n, mean =  eta, sd = 0.5)
+
+df = data.frame(y = y, 
+                x_smooth = inla.group(x)) # equidistant x's 
+
+
+
+## ----define_components_gam----------------------------------------------------
+cmp =  ~ Intercept(1) + 
+  smooth(x_smooth, model = "rw1")
+
+
+## ----define_likelihood_gam----------------------------------------------------
+lik =  bru_obs(formula = y ~.,
+            family = "gaussian",
+            data = df)
+
+
+## ----run_model_gam------------------------------------------------------------
+fit = bru(cmp, lik)
+fit$summary.fixed
+
+
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+#| 
+# data.frame(fit$summary.random$smooth) %>%
+#   ggplot() +
+#   geom_ribbon(aes(ID,ymin = X0.025quant, ymax= X0.975quant), alpha = 0.5) +
+#   geom_line(aes(ID,mean)) +
+#   xlab("covariate") + ylab("")
+
+
+## ----get_predictions_gam------------------------------------------------------
+pred = predict(fit, df, ~ (Intercept + smooth))
+
+
+## ----plot_gam-----------------------------------------------------------------
+#| code-fold: true
+#| fig-cap: Data and 95% credible intervals
+pred %>% ggplot() + 
+  geom_point(aes(x_smooth,y), alpha = 0.3) +
+  geom_line(aes(x_smooth,1+cos(x_smooth)),col=2)+
+  geom_line(aes(x_smooth,mean)) +
+  geom_line(aes(x_smooth, q0.025), linetype = "dashed")+
+  geom_line(aes(x_smooth, q0.975), linetype = "dashed")+
+  xlab("Covariate") + ylab("Observations")
 
 
