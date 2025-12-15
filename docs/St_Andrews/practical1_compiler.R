717671e981fcf097f037803e5f7d3cfc18f42998
JBelmont89 <38103692+JBelmont89@users.noreply.github.com>
2025-12-15 18:11:17 +0000

    fix_BES_typos

---
+++ docs/St_Andrews/practical1_compiler.R
@@ -0,0 +1,402 @@
+## ----child="practicals\\LM_ex.qmd"--------------------------------------------
+
+## -----------------------------------------------------------------------------
+#| warning: false
+#| message: false
+#| code-summary: "Load libraries"
+
+library(dplyr)
+library(INLA)
+library(ggplot2)
+library(patchwork)
+library(inlabru)     
+# load some libraries to generate nice plots
+library(scico)
+
+
+## -----------------------------------------------------------------------------
+#| code-fold: show
+#| code-summary: "Simulate Data from a LM"
+
+# set seed for reproducibility
+set.seed(1234) 
+
+beta = c(2,0.5)
+sd_error = 0.1
+
+n = 100
+x = rnorm(n)
+y = beta[1] + beta[2] * x + rnorm(n, sd = sd_error)
+
+df = data.frame(y = y, x = x)  
+
+
+
+
+
+
+
+
+
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Fit LM in `inlabru`"
+fit.lm = bru(cmp, lik)
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Model summaries"
+#| collapse: true
+summary(fit.lm)
+
+
+## -----------------------------------------------------------------------------
+new_data = data.frame(x = c(df$x, runif(10)),
+                      y = c(df$y, rep(NA,10)))
+pred = predict(fit.lm, new_data, ~ beta_0 + beta_1,
+               n.samples = 1000)
+
+
+
+
+
+## -----------------------------------------------------------------------------
+#| code-fold: true
+#| fig-cap: Data and 95% credible intervals
+#| echo: false
+#| message: false
+#| warning: false
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+
+pred %>% ggplot() + 
+  geom_point(aes(x,y), alpha = 0.3) +
+  geom_line(aes(x,mean)) +
+  geom_line(aes(x, q0.025), linetype = "dashed")+
+  geom_line(aes(x, q0.975), linetype = "dashed")+
+  xlab("Covariate") + ylab("Observations")
+
+
+
+
+
+
+## -----------------------------------------------------------------------------
+
+pred2 = predict(fit.lm, new_data, 
+               formula = ~ {
+                 mu = beta_0 + beta_1
+                 sigma = sqrt(1/Precision_for_the_Gaussian_observations)
+                 list(q1 = qnorm(0.025, mean = mu, sd = sigma),
+                      q2 =  qnorm(0.975, mean = mu, sd = sigma))},
+               n.samples = 1000)
+round(c(pred2$q1$mean, pred2$q2$mean),2)
+
+
+## -----------------------------------------------------------------------------
+#| eval: true
+#| purl: true 
+
+inla.priors.used(fit.lm)
+
+
+
+
+
+
+## -----------------------------------------------------------------------------
+
+# First we define the logGamma (0.01,0.01) prior 
+
+prec.tau <- list(prec = list(prior = "loggamma",   # prior name
+                             param = c(0.01, 0.01))) # prior values
+
+lik2 =  bru_obs(formula = y ~.,
+                family = "gaussian",
+                data = df,
+                control.family = list(hyper = prec.tau))
+
+fit.lm2 = bru(cmp2, lik2) 
+
+
+
+## -----------------------------------------------------------------------------
+#| fig-width: 4
+#| fig-align: center
+#| fig-height: 4
+#| code-fold: show
+
+plot(fit.lm, "beta_0")
+
+
+
+
+
+
+
+
+
+
+## -----------------------------------------------------------------------------
+data("iris")
+iris %>% ggplot() +
+  geom_point(aes(Sepal.Length, Petal.Length, color= Species)) +
+  facet_wrap(.~Species)
+
+
+## -----------------------------------------------------------------------------
+mod1 = lm(Petal.Length ~ Species, data  = iris)
+summary(mod1)
+
+
+## -----------------------------------------------------------------------------
+cmp = ~   spp(
+  main = ~ Species,
+  model = "fixed"
+)
+
+
+
+
+## -----------------------------------------------------------------------------
+mod2 = lm(Petal.Length ~ Species:Sepal.Length, data = iris)
+mod2
+
+
+
+## ----child="practicals\\LMM_ex.qmd"-------------------------------------------
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Simulate data from a LMM"
+#| 
+set.seed(12)
+beta = c(1.5,1)
+sd_error = 1
+tau_group = 1
+
+n = 100
+n.groups = 5
+x = rnorm(n)
+v = rnorm(n.groups, sd = tau_group^{-1/2})
+y = beta[1] + beta[2] * x + rnorm(n, sd = sd_error) +
+  rep(v, each = 20)
+
+df = data.frame(y = y, x = x, j = rep(1:5, each = 20))  
+
+
+## ----plot_data_lmm------------------------------------------------------------
+#| code-fold: true
+#| fig-cap: Data for the linear mixed model example with 5 groups
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+
+ggplot(df) +
+  geom_point(aes(x = x, colour = factor(j), y = y)) +
+  theme_classic() +
+  scale_colour_discrete("Group")
+
+
+
+## ----define_components_lmm----------------------------------------------------
+# Define model components
+cmp =  ~ -1 + beta_0(1) + beta_1(x, model = "linear") +
+  u(j, model = "iid")
+
+
+## ----define_likelihood_lmm----------------------------------------------------
+# Construct likelihood
+lik =  bru_obs(formula = y ~.,
+            family = "gaussian",
+            data = df)
+
+
+## -----------------------------------------------------------------------------
+#| collapse: true
+#| code-summary: "Fit a LMM in inlabru"
+fit = bru(cmp, lik)
+summary(fit)
+
+
+## -----------------------------------------------------------------------------
+#| code-fold: true
+#| code-summary: "LMM fitted values"
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+
+# New data
+xpred = seq(range(x)[1], range(x)[2], length.out = 100)
+j = 1:n.groups
+pred_data = expand.grid(x = xpred, j = j)
+pred = predict(fit, pred_data, formula = ~ beta_0 + beta_1 + u) 
+
+
+pred %>%
+  ggplot(aes(x=x,y=mean,color=factor(j)))+
+  geom_line()+
+  geom_ribbon(aes(x,ymin = q0.025, ymax= q0.975,fill=factor(j)), alpha = 0.5) + 
+  geom_point(data=df,aes(x=x,y=y,colour=factor(j)))+
+  facet_wrap(~j)
+
+
+
+
+## ----child="practicals\\GLM_ex.qmd"-------------------------------------------
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Simulate Data from a GLM"
+set.seed(123)
+n = 100
+beta = c(1,1)
+x = rnorm(n)
+lambda = exp(beta[1] + beta[2] * x)
+y = rpois(n, lambda  = lambda)
+df = data.frame(y = y, x = x)  
+
+
+
+
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "GLM likelihood"
+
+lik =  bru_obs(formula = eta,
+            family = "poisson",
+            data = df)
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Fit a GLM"
+fit_glm = bru(cmp, lik)
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "GLM summaries"
+summary(fit_glm)
+
+
+## ----get_predictions_glm------------------------------------------------------
+#| code-summary: "Predcited values for Poisson GLM"
+
+# Define new data, set to NA the values for prediction
+
+new_data = data.frame(x = c(df$x, runif(10)),
+                      y = c(df$y, rep(NA,10)))
+
+# Define predictor formula
+pred_fml <- ~ exp(beta_0 + beta_1)
+
+# Generate predictions
+pred_glm <- predict(fit_glm, new_data, pred_fml)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| code-summary: "Plot GLM predicted values"
+#| fig-cap: Data and 95% credible intervals
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+#| warning: false
+
+pred_glm %>% ggplot() + 
+  geom_point(aes(x,y), alpha = 0.3) +
+  geom_line(aes(x,mean)) +
+    geom_ribbon(aes(x = x, ymax = q0.975, ymin = q0.025),fill = "tomato", alpha = 0.3)+
+  geom_line(aes(x, q0.025), linetype = "dashed")+
+  geom_line(aes(x, q0.975), linetype = "dashed")+
+  xlab("Covariate") + ylab("Observations (counts)")
+
+
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "GLM Task"
+set.seed(123)
+n = 100
+alpha = c(0.5,1.5)
+w = rnorm(n)
+psi = plogis(alpha[1] + alpha[2] * w)
+y = rbinom(n = n, size = 1, prob =  psi) # set size = 1 to draw binary observations
+df_logis = data.frame(y = y, w = w)  
+
+
+
+## ----child="practicals\\GAM_ex.qmd"-------------------------------------------
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| message: false
+#| warning: false
+library(webexercises)
+library(dplyr)
+library(INLA)
+library(ggplot2)
+library(patchwork)
+library(inlabru)     
+# load some libraries to generate nice map plots
+library(scico)
+
+
+## -----------------------------------------------------------------------------
+#| code-summary: "Simulate GAM Data"
+
+set.seed(123)
+n = 100
+x = rnorm(n)
+eta = (1 + cos(x))
+y = rnorm(n, mean =  eta, sd = 0.5)
+
+df = data.frame(y = y, 
+                x_smooth = inla.group(x)) # equidistant x's 
+
+
+
+## ----define_components_gam----------------------------------------------------
+cmp =  ~ Intercept(1) + 
+  smooth(x_smooth, model = "rw1")
+
+
+## ----define_likelihood_gam----------------------------------------------------
+lik =  bru_obs(formula = y ~.,
+            family = "gaussian",
+            data = df)
+
+
+## ----run_model_gam------------------------------------------------------------
+fit = bru(cmp, lik)
+fit$summary.fixed
+
+
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+#| 
+# data.frame(fit$summary.random$smooth) %>%
+#   ggplot() +
+#   geom_ribbon(aes(ID,ymin = X0.025quant, ymax= X0.975quant), alpha = 0.5) +
+#   geom_line(aes(ID,mean)) +
+#   xlab("covariate") + ylab("")
+
+
+## ----get_predictions_gam------------------------------------------------------
+pred = predict(fit, df, ~ (Intercept + smooth))
+
+
+## ----plot_gam-----------------------------------------------------------------
+#| code-fold: true
+#| fig-cap: Data and 95% credible intervals
+pred %>% ggplot() + 
+  geom_point(aes(x_smooth,y), alpha = 0.3) +
+  geom_line(aes(x_smooth,1+cos(x_smooth)),col=2)+
+  geom_line(aes(x_smooth,mean)) +
+  geom_line(aes(x_smooth, q0.025), linetype = "dashed")+
+  geom_line(aes(x_smooth, q0.975), linetype = "dashed")+
+  xlab("Covariate") + ylab("Observations")
+
+
---
+++ docs/St_Andrews/practical2_compiler.R
@@ -0,0 +1,477 @@
+## ----child="practicals\\spatial_data_types_areal.qmd"-------------------------
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+library(CARBayesdata)
+
+data(pollutionhealthdata)
+data(GGHB.IZ)
+
+
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+library(dplyr)
+library(INLA)
+library(ggplot2)
+library(patchwork)
+library(inlabru)   
+library(mapview)
+library(sf)
+
+# load some libraries to generate nice map plots
+library(scico)
+
+
+## -----------------------------------------------------------------------------
+resp_cases <- merge(GGHB.IZ %>%
+                      mutate(space = 1:dim(GGHB.IZ)[1]),
+                             pollutionhealthdata, by = "IZ")%>%
+  dplyr::filter(year == 2007) 
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+resp_cases <- resp_cases %>% 
+  mutate(SMR = observed/expected )
+
+
+## -----------------------------------------------------------------------------
+#| fig-width: 5
+#| fig-height: 5
+#| fig-align: center
+#| code-fold: show
+ggplot()+
+  geom_sf(data=resp_cases,aes(fill=SMR))+
+  scale_fill_scico(palette = "roma")
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+
+library(spdep)
+
+W.nb <- poly2nb(GGHB.IZ,queen = TRUE)
+W.nb
+
+
+
+
+## -----------------------------------------------------------------------------
+library(spdep)
+R <- nb2mat(W.nb, style = "B", zero.policy = TRUE)
+diag = apply(R,1,sum)
+Q = -R
+diag(Q) = diag
+
+
+## -----------------------------------------------------------------------------
+#| echo: true
+#| eval: false
+
+# 
+# cmp = ~ Intercept(1) + space(...) + iid(...)
+# 
+# formula = ...
+# 
+# 
+# lik = bru_obs(formula = formula,
+#               family = ...,
+#               E = ...,
+#               data = ...)
+# 
+# fit = bru(cmp, lik)
+# 
+
+
+
+
+## -----------------------------------------------------------------------------
+#| echo: true
+#| eval: false
+
+# pred = predict(fit, resp_cases, ~data.frame(log_risk = ...,
+#                                              risk = exp(...),
+#                                              cases = ...
+#                                              ),
+#                n.samples = 1000)
+# 
+
+
+
+
+## -----------------------------------------------------------------------------
+#| fig-width: 4
+#| fig-height: 4
+#| fig-align: center
+pred$cases %>% ggplot() + geom_point(aes(observed, mean)) + 
+  geom_errorbar(aes(observed, ymin = q0.025, ymax = q0.975)) +
+  geom_abline(intercept = 0, slope = 1)
+
+
+
+
+## ----child="practicals\\spatial_data_types_geostats.qmd"----------------------
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+#| echo: false
+
+library(dplyr)
+library(INLA)
+library(ggplot2)
+library(patchwork)
+library(inlabru)   
+library(mapview)
+library(sf)
+
+# load some libraries to generate nice map plots
+library(scico)
+
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+library(sdmTMB)
+
+pcod_df = sdmTMB::pcod %>% filter(year==2003)
+qcs_grid = sdmTMB::qcs_grid
+
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+#| 
+pcod_sf =   st_as_sf(pcod_df, coords = c("lon","lat"), crs = 4326)
+pcod_sf = st_transform(pcod_sf,
+                       crs = "+proj=utm +zone=9 +datum=WGS84 +no_defs +type=crs +units=km" )
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+library(terra)
+depth_r <- rast(qcs_grid, type = "xyz")
+crs(depth_r) <- crs(pcod_sf)
+
+
+## -----------------------------------------------------------------------------
+#| message: false
+#| warning: false
+#| code-fold: show
+#| fig-align: center
+
+library(tidyterra)
+ggplot()+ 
+  geom_spatraster(data=depth_r$depth)+
+  geom_sf(data=pcod_sf,aes(color=factor(present))) +
+    scale_color_manual(name="Occupancy status for the Pacific Cod",
+                     values = c("black","orange"),
+                     labels= c("Absence","Presence"))+
+  scale_fill_scico(name = "Depth",
+                   palette = "nuuk",
+                   na.value = "transparent" ) + xlab("") + ylab("")
+
+
+
+## -----------------------------------------------------------------------------
+#| fig-align: center
+#| fig-width: 5
+#| fig-height: 5
+mesh = fm_mesh_2d(loc = pcod_sf,           # Build the mesh
+                  cutoff = 2,
+                  max.edge = c(7,20),     # The largest allowed triangle edge length.
+                  offset = c(5,50))       # The automatic extension distance
+
+ggplot() + gg(mesh) +
+  geom_sf(data= pcod_sf, aes(color = factor(present))) + 
+  xlab("") + ylab("")
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+
+# ?fm_mesh_2d
+
+
+## -----------------------------------------------------------------------------
+spde_model1 =  inla.spde2.pcmatern(mesh,
+                                  prior.sigma = c(.1, 0.5),
+                                  prior.range = c(30, 0.5))
+spde_model2 =  inla.spde2.pcmatern(mesh,
+                                  prior.sigma = c(10, 0.5),
+                                  prior.range = c(1000, 0.5))
+spde_model3 =  inla.spde2.pcmatern(mesh,
+                                  prior.sigma = c(1, 0.5),
+                                  prior.range = c(100, 0.5))
+
+
+## -----------------------------------------------------------------------------
+cmp = ~ Intercept(1) + space(geometry, model = spde_model3)
+
+
+## -----------------------------------------------------------------------------
+
+formula = present ~ Intercept  + space
+
+lik = bru_obs(formula = formula, 
+              data = pcod_sf, 
+              family = "binomial")
+
+
+
+## -----------------------------------------------------------------------------
+fit1 = bru(cmp,lik)
+
+
+
+
+## -----------------------------------------------------------------------------
+pxl = fm_pixels(mesh)
+
+
+## -----------------------------------------------------------------------------
+preds = predict(fit1, pxl, ~data.frame(spatial = space,
+                                      total = Intercept + space))
+
+
+## -----------------------------------------------------------------------------
+#| fig-align: center
+#| code-fold: show
+
+ggplot() + geom_sf(data = preds$spatial,aes(color = mean)) + 
+  scale_color_scico() +
+  ggtitle("Posterior mean") +
+ggplot() + geom_sf(data = preds$spatial,aes(color = sd)) + 
+  scale_color_scico() +
+  ggtitle("Posterior sd")
+
+
+
+
+## -----------------------------------------------------------------------------
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+# create the grouped variable
+depth_r$depth_group = inla.group(values(depth_r$depth_scaled))
+
+# run the model
+cmp = ~ Intercept(1) + space(geometry, model = spde_model3) +
+        covariate(depth_r$depth_group, model = "rw2")
+
+formula = present ~ Intercept  + space + covariate
+
+lik = bru_obs(formula = formula, 
+              data = pcod_sf, 
+              family = "binomial")
+
+
+fit3 = bru(cmp, lik)
+
+# plot the estimated effect of depth
+
+fit3$summary.random$covariate %>% 
+  ggplot() + geom_line(aes(ID,mean)) + 
+             geom_ribbon(aes(ID,
+                             ymin = `0.025quant`, 
+                             ymax = `0.975quant`),
+                         alpha = 0.5)
+
+
+## -----------------------------------------------------------------------------
+pxl1 = data.frame(crds(depth_r), 
+                  as.data.frame(depth_r$depth)) %>% 
+       filter(!is.na(depth)) %>%
+st_as_sf(coords = c("x","y")) %>% dplyr::select(-depth)
+
+
+## -----------------------------------------------------------------------------
+inv_logit = function(x) (1+exp(-x))^(-1)
+
+
+
+
+## ----child="practicals\\spatial_data_types_PP.qmd"----------------------------
+
+## -----------------------------------------------------------------------------
+#| fig-cap: "Distribution of the observed forest fires caused by lightning in Castilla-La Mancha in 2004"
+#| 
+data("clmfires")
+pp = st_as_sf(as.data.frame(clmfires) %>%
+                dplyr::mutate(x = x, 
+                       y = y),
+              coords = c("x","y"),
+              crs = NA) %>%
+  dplyr::filter(cause == "lightning",
+         year(date) == 2004)
+
+poly = as.data.frame(clmfires$window$bdry[[1]]) %>%
+  mutate(ID = 1)
+
+region = poly %>% 
+  st_as_sf(coords = c("x", "y"), crs = NA) %>% 
+  dplyr::group_by(ID) %>% 
+  summarise(geometry = st_combine(geometry)) %>%
+  st_cast("POLYGON") 
+  
+ggplot() + geom_sf(data = region, alpha = 0) + geom_sf(data = pp)  
+
+
+## -----------------------------------------------------------------------------
+
+elev_raster = rast(clmfires.extra[[2]]$elevation)
+elev_raster = scale(elev_raster)
+
+
+
+
+
+## -----------------------------------------------------------------------------
+#| fig-width: 4
+#| fig-height: 4
+#| fig-align: center
+# mesh options
+
+mesh <-  fm_mesh_2d(boundary = region,
+                  max.edge = c(5, 10),
+                  cutoff = 4, crs = NA)
+
+ggplot() + gg(mesh) + geom_sf(data=pp)
+
+
+## -----------------------------------------------------------------------------
+st_area(region)
+
+
+## -----------------------------------------------------------------------------
+spde_model =  inla.spde2.pcmatern(mesh,
+                       prior.sigma = c(1, 0.5), 
+                       prior.range = c(100, 0.5)) 
+
+
+
+
+
+## -----------------------------------------------------------------------------
+# build integration scheme
+ips = fm_int(mesh,
+             samplers = region)
+
+
+
+## -----------------------------------------------------------------------------
+eval_spatial(elev_raster,pp) %>% is.na() %>% any()
+eval_spatial(elev_raster,ips) %>% is.na() %>% any()
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| fig-align: center
+#| fig-width: 4.5
+#| fig-height: 4.5
+#| label: fig-points
+#| fig-cap: "Integration scheme for numerical approximation of the stochastic integral in La Mancha Region"
+ggplot()+tidyterra::geom_spatraster(data=elev_raster)+
+  geom_sf(data=ips,alpha=0.25,col=1)+
+   geom_sf(data=ips[3525,],col=2)
+
+
+## -----------------------------------------------------------------------------
+# Extend raster ext by 5 % of the original raster
+re <- extend(elev_raster, ext(elev_raster)*1.05)
+# Convert to an sf spatial object
+re_df <- re %>% stars::st_as_stars() %>%  st_as_sf(na.rm=F)
+# fill in missing values using the original raster 
+re_df$lyr.1 <- bru_fill_missing(elev_raster,re_df,re_df$lyr.1)
+# rasterize
+elev_rast_p <- stars::st_rasterize(re_df) %>% rast()
+
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| fig-align: center
+#| fig-width: 4.5
+#| fig-height: 4.5
+ggplot()+tidyterra::geom_spatraster(data=elev_rast_p)+
+  geom_sf(data=ips,alpha=0.25,col=1)+
+   geom_sf(data=ips[3525,],col=2)
+
+
+## -----------------------------------------------------------------------------
+cmp_lgcp <-  geometry ~  Intercept(1)  + 
+  elev(elev_rast_p, model = "linear") +
+  space(geometry, model = spde_model)
+
+
+## -----------------------------------------------------------------------------
+formula = geometry ~ Intercept  + elev + space
+
+
+## -----------------------------------------------------------------------------
+lik = bru_obs(formula = formula, 
+              data = pp, 
+              family = "cp",
+              ips = ips)
+
+
+
+
+
+## -----------------------------------------------------------------------------
+fit_lgcp = bru(cmp_lgcp,lik)
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+
+# summary(fit_lgcp)
+
+
+
+
+## -----------------------------------------------------------------------------
+elev_crop <- terra::crop(x = elev_raster,y = region,mask=TRUE)
+
+
+pxl1 = data.frame(crds(elev_crop), 
+                  as.data.frame(elev_crop$lyr.1)) %>% 
+       filter(!is.na(lyr.1)) %>%
+st_as_sf(coords = c("x","y")) %>%
+  dplyr::select(-lyr.1)
+
+
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+
+# lgcp_pred <- predict(
+#   fit_lgcp,
+#   pxl1,
+#   ~ data.frame(
+#     lambda = exp(Intercept + elev + space), # intensity
+#     loglambda = Intercept + elev +space,  #log-intensity
+#     GF = space # matern field
+#   )
+# )
+# 
+# # predicted log intensity
+# ggplot() + gg(lgcp_pred$loglambda, geom = "tile")
+# # standard deviation of the predicted log intensity
+# ggplot() + gg(lgcp_pred$loglambda, geom = "tile",aes(fill=sd))
+# # predicted intensity
+# ggplot() +  gg(lgcp_pred$lambda, geom = "tile")
+# # spatial field
+# ggplot() +  gg(lgcp_pred$GF, geom = "tile")
+
+
